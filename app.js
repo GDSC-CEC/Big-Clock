18bd941f50fb4bf40c0bf544f92130598967e880
Vipul Kumar <vipulkmr02@gmail.com>
2022-12-29 20:14:56 +0530

    Dark-mode added

--- app.js
+++ app.js
@@ -21,7 +21,30 @@ function set_seperator(str) {
     $('.sep').text(str);
 }
 
+function dark_theme(d) {
+    // 1 for turn on dark theme
+    if (d == 1) {
+        $('body').removeClass('back-light');
+        $('body').addClass('back-dark');
+        $('.main-text').removeClass("main-light");
+        $('.main-text').addClass("main-dark");
+        $('.sec-text').removeClass("sec-light");
+        $('.sec-text').addClass("sec-dark");
+    }
+    
+    else if (d == 0) {
+        $('body').removeClass('back-dark');
+        $('body').addClass('back-light');
+        $('.sec-text').removeClass("sec-dark");
+        $('.sec-text').addClass("sec-light");
+    }
+
+}
+
 setInterval(update_time, 1000);
 
 set_seperator(" : ");
+
+dark_theme(1);
+
 
